c7910005a2f4ce8a3963eace3bde7fd106a9a08d
Fahad Algharazi <fahad.algharazi@csedge.org>
2019-12-09 16:04:47 -0500

    updated componenets

--- great-app/src/app/people.ts
+++ great-app/src/app/people.ts
@@ -5,17 +5,17 @@ caption: string;
 link: string;	
 } 
 
-export const PEOPLE:Person[]=[{
+export const PEOPLE:[]=[{
 	name: 'Heyreddin',
 	picture: 'https://www.thefamouspeople.com/profiles/images/og-hayreddin-barbarossa-41370.jpg',
-	caption: 'a',
-	link: ''
+	caption: 'The admiral of the Ottoman navy who won the battle for the mediterranean, the Battle of Preveza one of the three largest sea battles within the 16th centuary Mediterranean.Against immense odds admiral Barbarossa defeated the Holy League through ingenious manouvers that cought the Holy League of gaurd.The Holy League was a coalation of the Papal States,the Republic of Venice,the Maltese Knights and Spain including its vassal states of Naples and Sicily.In total the Holy league assembled a fleet of around 300 ships that were larger and able to inflict considerable damage to the smaller Ottomen Ships that numbered about 120 .',
+	link: 'https://en.wikipedia.org/wiki/Hayreddin_Barbarossa'
 },
 {
 	name: 'Heyreddin',
 	picture: 'https://www.thefamouspeople.com/profiles/images/og-hayreddin-barbarossa-41370.jpg',
-	caption: 'a',
-	link: ''
+	caption: 'The admiral of the Ottoman navy who won the battle for the mediterranean, the Battle of Preveza one of the three largest sea battles within the 16th centuary Mediterranean.Against immense odds admiral Barbarossa defeated the Holy League through ingenious manouvers that cought the Holy League of gaurd.The Holy League was a coalation of the Papal States,the Republic of Venice,the Maltese Knights and Spain including its vassal states of Naples and Sicily.In total the Holy league assembled a fleet of around 300 ships that were larger and able to inflict considerable damage to the smaller Ottomen Ships that numbered about 120 .',
+	link: 'https://en.wikipedia.org/wiki/Hayreddin_Barbarossa'
 },
 {
 	name: 'Heyreddin',
